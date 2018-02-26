05f0d22a5d83daea4f4c50d931af75ab5cd96d4e
VincentSastra <33113453+VincentSastra@users.noreply.github.com>
2018-02-26 10:26:06 +0700

    Create 2018_02_26_101800_create_heats_table.php
    Created heats table migration

---
+++ database/migrations/2018_02_26_101800_create_heats_table.php
@@ -0,0 +1,30 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateMeetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('heats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->increments('events_id');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('heats');
+    }
+}
