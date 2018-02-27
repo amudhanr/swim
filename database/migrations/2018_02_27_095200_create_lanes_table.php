ed78a7f1c7c283baef5f77089b36368398097d8b
VincentSastra <33113453+VincentSastra@users.noreply.github.com>
2018-02-27 10:06:54 +0700

    Create 2018_02_27_095200_create_lanes_table.php
    Created lanes table migration

---
+++ database/migrations/2018_02_27_095200_create_lanes_table.php
@@ -0,0 +1,37 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateMeetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lanes', function (Blueprint $table) {
+            $table->increments('id');
+	          $table->integer('heats_id')->unsigned();
+	          $table->foreign('heats_id')->references('id')->on('heats');
+            $table->tinyInteger('lane_number')->unsigned();
+            $table->integer('swimmers_id')->unsigned();
+            $table->foreign('swimmers_id')->references('id')->on('swimmers');
+            $table->integer('seed_time')->unsigned();
+            $table->integer('time')->unsigned();
+            $table->tinyInteger('position')->unsigned();
+            $table->tinyInteger('points')->unsigned();
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lanes');
+    }
+}
