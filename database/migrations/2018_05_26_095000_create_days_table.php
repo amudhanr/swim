756bab70644bb866fd78eaf1dbbee501a1193f4c
VincentSastra <33113453+VincentSastra@users.noreply.github.com>
2018-03-01 10:17:55 +0700

    Update 2018_05_26_095000_create_days_table.php
    add ()

--- database/migrations/2018_05_26_095000_create_days_table.php
+++ database/migrations/2018_05_26_095000_create_days_table.php
@@ -13,7 +13,7 @@ class CreateDaysTable extends Migration
     {
         Schema::create('days', function (Blueprint $table) {
             $table->increments('id');
-	    $table->integer('meets_id')->unsigned;
+	    $table->integer('meets_id')->unsigned();
 	    $table->foreign('meets_id')->references('id')->on('meets');
             $table->string('youtube_link');
 	    $table->date('date');
