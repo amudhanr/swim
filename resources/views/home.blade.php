c91027bc0883549cfe718f040ce5fb6dab4f51fa
Jessica Hirianto <jessicahirianto>
2018-03-01 10:18:29 +0700

    Change the welcome layout to use the layouts.app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="container">
+	<div class="row">
+    	
+    </div>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
