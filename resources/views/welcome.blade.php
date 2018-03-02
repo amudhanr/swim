875bc33feaff025b6b60f16974114d9b042701ef
Hamin Noh <haminnoh>
2018-03-02 10:23:47 +0700

    I made 2 columns with ranking and video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,18 @@
 @section('content')
 <div class="container">
 	<div class="row">
-    	
+		    	
     </div>
+    <div class="row">
+    	<div class="col-sm-6" style="background-color:lavender;">	
+        	<h2 class ="text-center"> Ranking </h2>
+            
+         </div>
+        <div class="col-sm-6" style="background-color:lavender;">
+       		<h2 class ="text-center"> Video </h2>
+            <iframe width="450" height="340" src ="https://www.youtube.com/watch?v=lMM61mH0dJo" frameborder="0">
+        </div>
+     </div>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
