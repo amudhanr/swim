ea8624ab648f59d0eba7d327617a66c26f092c30
VincentSastra <33113453+VincentSastra@users.noreply.github.com>
2018-03-06 08:41:23 +0700

    Update create.blade.php

--- resources/views/hosts/create.blade.php
+++ resources/views/hosts/create.blade.php
@@ -1,4 +1,13 @@
 @extends('layout.app')
-
+ 
+  <form method="post" action="/admin/hosts">
+  
+    <input type="text" name="name" placeholder="Enter name">
+    
+    <input type="text" name="address" placeholder="Enter address">
+    
+    <input type="submit" name="submit">
+    
+  </form>
 
 @section('content')
