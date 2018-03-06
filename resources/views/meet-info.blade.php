6d2b4adc681a7f51d58ab7ffc0eedc7edf8ad620
heeminjung <heeminjung@baisedu.org>
2018-03-06 10:23:31 +0700

    I added Day 2

--- resources/views/meet-info.blade.php
+++ resources/views/meet-info.blade.php
@@ -2,11 +2,22 @@
 
 @section('content')
 <div class="container">
-<h1>MAIN EVENT</h1>
+<h1 class ="text-center">MAIN EVENT</h1>
 	<div class="row">
-     <div class="col-sm-8" style="background-color:lavenderblush;">.</div>
-    <div class="col-sm-4" style="background-color:lavender;">.</div>
-        
+     <div class="col-sm-8" style="background-color:lavenderblush;">
+     <h4> DAY 1</h4>
+     </div>
+    <div class="col-sm-4" style="background-color:lavender;">
+           	<iframe width="340" height="340" src ="https://www.youtube.com/watch?v=lMM61mH0dJo" frameborder="0"></iframe>
+              </div>
+    </div>
+   	<div class="row">
+     <div class="col-sm-8" style="background-color:lavenderblush;">.
+     <h4> DAY 2</h4>
+     </div>
+    <div class="col-sm-4" style="background-color:lavender;">.
+        	<iframe width="340" height="340" src ="https://www.youtube.com/watch?v=lMM61mH0dJo" frameborder="0"></iframe>
+    </div>
     </div>
 </div>
 @endsection
