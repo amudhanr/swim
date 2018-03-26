084d5ed50925803886bf8b9bb9b81d0cf5b61b60
junseonh <36021736+junseonh@users.noreply.github.com>
2018-03-26 10:23:20 +0700

    Create TeamsController.php

---
+++ app/Http/Controllers/TeamsController.php
@@ -0,0 +1,92 @@
+<?php
+namespace App\Http\Controllers;
+use App\Teams;
+use Illuminate\Http\Request;
+class TeamsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('teams.create');//
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       /**$rules = array(
+		'name'		=> 'required',
+		'address'	=> 'required|email'
+	);
+	$validator = Validator::make(Input::all(), $rule);
+	if ($validator->fails()) {
+		return Redirect::to('admin/teams/create')
+			->withErrors($validator)
+			->withInput;
+	} else {
+	**/
+	$teams = new Team;
+	$teams->name	= $request->name;
+	$teams->shortName = $request->shortName;
+	$teams->save();
+	return "DONE!";
+	
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
