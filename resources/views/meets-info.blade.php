782d634925cb4b7e41fbee10c8a0fcad0a0a4f67
heeminjung <36021573+heeminjung@users.noreply.github.com>
2018-03-27 09:46:06 +0700

    Update meets-info.blade.php

--- resources/views/meets-info.blade.php
+++ resources/views/meets-info.blade.php
@@ -6,6 +6,12 @@
 	<div class="row">
      <div class="col-sm-8" style="background-color:lavenderblush;">
      <h4> DAY 1</h4>
+     <p>Event #1 25m freestyle Age 10-11 (boys)</p>
+     <p>Event #2 25m freestyle Age 10-11 (girls)</p>
+     <p>Event #3 50m freestyle Age 12-13 (boys)</p>
+     <p>Event #4 50m freestyle Age 12-13 (girls)</p>
+     <p>Event #5 50m freestyle Age 14-15 (boys)</p>
+     <p>Event #6 50m freestyle Age 14-15 (girls)</p>
      </div>
     <div class="col-sm-4" style="background-color:lavender;">
            	<iframe width="340" height="340" src ="https://www.youtube.com/watch?v=lMM61mH0dJo" frameborder="0"></iframe>
@@ -14,6 +20,12 @@
    	<div class="row">
      <div class="col-sm-8" style="background-color:lavenderblush;">.
      <h4> DAY 2</h4>
+     <p>Event #1 25m backstroke Age 10-11 (boys)</p>
+     <p>Event #2 25m backstroke Age 10-11 (girls)</p>
+     <p>Event #3 50m backstroke Age 12-13 (boys)</p>
+     <p>Event #4 50m backstroke Age 12-13 (girls)</p>
+     <p>Event #5 50m backstroke Age 14-15 (boys)</p>
+     <p>Event #6 50m backstroke Age 14-15 (girls)</p>
      </div>
     <div class="col-sm-4" style="background-color:lavender;">.
         	<iframe width="340" height="340" src ="https://www.youtube.com/watch?v=lMM61mH0dJo" frameborder="0"></iframe>
