b860283109feab81fc5caaa71aa8aa994f45a9c6
VincentSastra <33113453+VincentSastra@users.noreply.github.com>
2018-03-27 10:18:52 +0700

    Create DaysController.php
    Finish Days controller including the foreign id (views still dont have foreign id as of now)

---
+++ app/Http/Controllers/DaysController.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Http\Controllers;
+use App\Teams;
+use Illuminate\Http\Request;
+class TeamsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('days.create');//
+    }
+   
+	$days = new Day;
+  $days->meets_id = $request->meets_id;
+	$days->youtube_link	= $request->youtube_link;
+	$days->date = $request->date;
+	$days->slug = $request->slug;
+	  
+	$teams->save();
+	return "DONE!";
+	
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
