8b01427db364c236ff9af499eaf70766e9b4dce5
seonhoikang <36021676+seonhoikang@users.noreply.github.com>
2018-03-27 10:04:42 +0700

    Update create.blade.php
    I added slug, start date, end date, and slect hosts id.

--- resources/views/meets/create.blade.php
+++ resources/views/meets/create.blade.php
@@ -9,7 +9,19 @@
       
       <input type="text" name="address" placeholder="Enter address">
       
+      <input type="text" name="slug" placeholder="unique identifier for the meet">
+      
+      <input type="text" name="start_date" placeholder="Start Date for the Meet">
+      
+      <input type="text" name="end_date" placeholder="End Date for the Meet">
+      
+      <select name="hosts_id">
+          <option value="##">Hosts Name</option>
+          //FIXME: The above options should be generated using a for loop based on the values stored in the hosts table
+      </select>
+      
       <input type="submit" name="submit">
+      
       
     </form>
   </div>
