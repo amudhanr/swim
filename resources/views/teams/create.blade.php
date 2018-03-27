5da68671d07d19712d60a8fb778ffab3e19995b2
junseonh <36021736+junseonh@users.noreply.github.com>
2018-03-27 09:54:48 +0700

    Update create.blade.php

--- resources/views/teams/create.blade.php
+++ resources/views/teams/create.blade.php
@@ -9,6 +9,16 @@
     
     <input type="text" name="shortName" placeholder="Enter shortName">
     
+    <input type="text" name="address" placeholder="Enter address">
+    
+    <input type="text" name="contactPerson" placeholder="Enter contactPerson">
+        
+    <input type="text" name="contactEmail" placeholder="Enter contactEmail">
+            
+    <input type="text" name="contactPhone" placeholder="Enter contactPhone">
+    
+    <input type="text" name="slug" placeholder="Enter slug">
+    
     <input type="submit" name="submit">
     
   </form>
