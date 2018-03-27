8c09a6defc4d634d8e79276a44eeab14687f0792
ruizhilin <36021692+ruizhilin@users.noreply.github.com>
2018-03-27 10:10:27 +0700

    missing inputs

---
+++ resources/views/heats/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <form method="post" action="/admin/heats/create">
+    
+     
+      
+      
+    </form>
+  </div>
+</div>
+@endsection
