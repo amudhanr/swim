89bee602ac777be65c5bb82230d9e3c17ebc959b
ruizhilin <36021692+ruizhilin@users.noreply.github.com>
2018-04-02 10:36:39 +0700

    Update uploadfile.blade.php

--- resources/views/uploadfile.blade.php
+++ resources/views/uploadfile.blade.php
@@ -1,17 +1,25 @@
-
-@extends('layouts.app')
-
-
-      
-@section('content')
-         // echo Form::open(array('url' => '/uploadfile','files'=>'true'));
-        // echo 'Select the file to upload.';
-         //echo Form::file('image');
-        // echo Form::submit('Upload File');
-        // echo Form::close();
-
-      
-      {!!Form::open(array('url' => '/uploadfile','files'=>'true'))!!}
-      {!!form::label('fileupload', 'File Upload:')!!}
-      <input id="input-1" type="file" class="file" name="filefield">
-@endsection
+<div class="about-section">
+   <div class="text-content">
+     <div class="span7 offset1">
+        @if(Session::has('success'))
+          <div class="alert-box success">
+          <h2>{!! Session::get('success') !!}</h2>
+          </div>
+        @endif
+        <div class="secure">Upload form</div>
+        {!! Form::open(array('url'=>'apply/upload','method'=>'POST', 'files'=>true)) !!}
+         <div class="control-group">
+          <div class="controls">
+          {!! Form::file('image') !!}
+      <p class="errors">{!!$errors->first('image')!!}</p>
+    @if(Session::has('error'))
+    <p class="errors">{!! Session::get('error') !!}</p>
+    @endif
+        </div>
+        </div>
+        <div id="success"> </div>
+      {!! Form::submit('Submit', array('class'=>'send-btn')) !!}
+      {!! Form::close() !!}
+      </div>
+   </div>
+</div>
