3499daab986baec799695688bf142de40446410e
ruizhilin <36021692+ruizhilin@users.noreply.github.com>
2018-04-03 09:36:11 +0700

    Update uploadfile.blade.php

--- resources/views/uploadfile.blade.php
+++ resources/views/uploadfile.blade.php
@@ -1,4 +1,4 @@
-<div class="about-section">
+//<div class="about-section">
    <div class="text-content">
      <div class="span7 offset1">
         @if(Session::has('success'))
@@ -22,4 +22,12 @@
       {!! Form::close() !!}
       </div>
    </div>
-</div>
+</div>//
+     <?php
+         echo Form::open(array('url' => '/uploadfile','files'=>'true'));
+         echo 'Select the file to upload.';
+         echo Form::file('image');
+         echo Form::submit('Upload File');
+         echo Form::close();
+      ?>
+   
