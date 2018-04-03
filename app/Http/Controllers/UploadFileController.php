443ff38a3611e27aad85227ccb49f1c63e1f70f0
ruizhilin <36021692+ruizhilin@users.noreply.github.com>
2018-04-03 09:53:50 +0700

    Update UploadFileController.php

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Http\Controllers\UploadFileController;
+use App\Http\Controllers\Controller;
 
 class UploadFileController extends Controller {
    public function index(){
