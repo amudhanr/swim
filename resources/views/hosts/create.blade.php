de7c2e88a11d34b291fdea8587b2f3df92f0bc18
Seon Ho Kang <seonhokang@baisedu.org>
2018-04-04 10:00:15 +0700

    made it look pretty

--- resources/views/hosts/create.blade.php
+++ resources/views/hosts/create.blade.php
@@ -1,16 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container">
   <form method="post" action="/admin/hosts">
  {{ csrf_field() }}
- 
+ <div class="form-group">
+	<label for="name">Enter Name</label>	
     <input type="text" name="name" placeholder="Enter name">
-
+ </div>
+ <div class="form-group">
+	<label for="slug">Enter Unique Identifier for the Meet</label>
     <input type="text" name="address" placeholder="Enter address">
-    
+ </div>
+ <div class="form-group">
     <input type="submit" name="submit">
-    
+ </div>
   </form>
+</div>
 
 @endsection
