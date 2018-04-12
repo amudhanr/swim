b4234d40c7e7c9663aa7528b2e5bc43b2dd0ea8e
Hamin Noh <haminnoh@baisedu.org>
2018-04-12 10:19:09 +0700

    Moving the event detail page under events folder and added a method stub to display event under EventsController

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -20,4 +20,15 @@ class EventsController extends Controller
 	return view('meets.index', ['events' => $events, 'days' => $daysData]);
         //FIXME: need to edit the above foreach loop to group the events by day
     }
+
+    public function event($event_id) {
+        $days = Meets::find($meet_id)->days;
+        $events = $daysData = array();
+        foreach ($days as $day) {
+            $events[$day->date][] = Days::find($day->id)->events;
+            $daysData[$day->date] = $day;
+        }
+	return view('meets.index', ['events' => $events, 'days' => $daysData]);
+        //FIXME: need to edit the above foreach loop to group the events by day
+    }
 }
