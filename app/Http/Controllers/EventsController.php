9b24664e3a803c0a52793a042631b4450d11a4f4
tjutbryan <bryantjut@baisedu.org>
2018-04-12 10:16:52 +0700

    retrieving the meets name from the database and pass it into the view

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -12,12 +12,13 @@ class EventsController extends Controller
 
     public function index($meet_id) {
         $days = Meets::find($meet_id)->days;
+        $meet = Meets::find($meet_id)->first();
         $events = $daysData = array();
         foreach ($days as $day) {
             $events[$day->date][] = Days::find($day->id)->events;
             $daysData[$day->date] = $day;
         }
-	return view('meets.index', ['events' => $events, 'days' => $daysData]);
+	return view('meets.index', ['events' => $events, 'days' => $daysData, 'meet' => $meet]);
         //FIXME: need to edit the above foreach loop to group the events by day
     }
 }
