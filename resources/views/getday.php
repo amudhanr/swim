53daa6a4592e5b8c105d2bac3f7043a5aa8e2451
ruizhilin <36021692+ruizhilin@users.noreply.github.com>
2018-04-20 10:23:42 +0700

    Update getday.php

--- resources/views/getday.php
+++ resources/views/getday.php
@@ -29,24 +29,17 @@ mysqli_select_db($con,"ajax_demo");
 $sql="SELECT * FROM user WHERE id = '".$q."'";
 $result = mysqli_query($con,$sql);
 
-echo "<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-<th>Age</th>
-<th>Hometown</th>
-<th>Job</th>
-</tr>";
-while($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['FirstName'] . "</td>";
-    echo "<td>" . $row['LastName'] . "</td>";
-    echo "<td>" . $row['Age'] . "</td>";
-    echo "<td>" . $row['Hometown'] . "</td>";
-    echo "<td>" . $row['Job'] . "</td>";
-    echo "</tr>";
+
+while($val = mysqli_fetch_array($result)) {
+    
+    echo "
+        <select>
+             <option value="1">$val['day1']</option>
+             <option value="2">$val['day1']</option>
+             <option value="3">$val['day1']</option>
+             <option value="4">$val['day1']</option>
+        </select>";
 }
-echo "</table>";
 mysqli_close($con);
 ?>
 </body>
