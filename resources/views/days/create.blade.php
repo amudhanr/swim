d3c3410fdf47934228a11bc72896443bef8abe94
SirGioLiMan <sergiolimandibhratha@baisedu.org>
2018-04-25 11:10:51 +0700

    fixed the location thingy

--- resources/views/days/create.blade.php
+++ resources/views/days/create.blade.php
@@ -6,7 +6,7 @@
     <form method="post" action="/admin/days/create">
 
 
-  {!! Form::open(array('url' => '/admin/meets/','ckass' => '')); !!} 
+  {!! Form::open(array('url' => '/admin/days/','class' => 'form-horizontal')); !!} 
     <div class="form-group">
 	<label for="id"> Enter ID</label>
 	<input type="text" name="id" id="id" placeholder="Enter ID" class="form-control">
@@ -16,7 +16,7 @@
 	<label for= "meets_id"> Enter Meets ID</label>
 	<input type="text" name="meets_id" id="meets_id" placeholder="Enter Meets ID" class ="form-control">
      </div>
-	
+        {!! Form::select("days", $days, null, array('class' => 'form-control')) !!} 
     <div class="form-group">
 	<label for="youtube_link"> Enter Youtube Link</label> 
 	<input type="text" name="youtube_link" id="youtube_link" placeholder="Enter YouTube Link" class="form-control">
@@ -32,7 +32,6 @@
       </div>
     <div class="form-group"> 
         {!! Form::submit('Add Day', array('placeholder' => 'btn btn-primary')); !!} 
-
     </form>
         {!! Form::close(); !!} 
   </div>
