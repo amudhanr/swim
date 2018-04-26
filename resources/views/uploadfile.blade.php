bf0fd0bb4bc5fa8fd75b1d3072751de0706699fd
ruizhilin <36021692+ruizhilin@users.noreply.github.com>
2018-04-26 10:22:55 +0700

    Update uploadfile.blade.php

--- resources/views/uploadfile.blade.php
+++ resources/views/uploadfile.blade.php
@@ -5,11 +5,9 @@ $(".meets").change(function(){
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      return this;
+      document.getElementById("day").innerHTML = this;
     }
   };
-  xmlhttp.open("GET", "cd_catalog.xml", true);
-  xmlhttp.send();
 }
 
 });
