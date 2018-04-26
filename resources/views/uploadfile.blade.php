a614cdc16900f3e4fd231f56f68a3465784ec9b3
ruizhilin <36021692+ruizhilin@users.noreply.github.com>
2018-04-26 10:26:03 +0700

    Update uploadfile.blade.php

--- resources/views/uploadfile.blade.php
+++ resources/views/uploadfile.blade.php
@@ -5,7 +5,7 @@ $(".meets").change(function(){
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("day").innerHTML = this.value;
+      alert("sdafasdfsa");
     }
   };
 }
