665100fff574d61cad2f78182f8fd0f5d6a394ac
administrator <amudhanr@baisedu.org>
2018-04-27 10:12:46 +0700

    Kind of fixed it but not really

--- resources/views/days/create.blade.php
+++ resources/views/days/create.blade.php
@@ -2,36 +2,27 @@
 
 @section('content')
 <div class="container">
-  <div class="row">
+<div class="row">
   {!! Form::open(array('url' => '/admin/days/','class' => 'form-horizontal')); !!} 
     <div class="form-group">
 	<label for="name"> Name</label>
 	<input type="text" name="name" id="name" placeholder="Insert Name" class="form-control">
-      </div>
-
+    </div>
     <div class="form-group">
-	<label for= "meets_id"> Enter Meets ID</label>
-	<input type="text" name="meets_id" id="meets_id" placeholder="Enter Meets ID" class ="form-control">
-     </div>
-	
+        <label for="meets_id">Meet Name:</label>
+        {!! Form::select('meets_id', $meets, null, array('class' => "form-control meets", 'selected' => "selected")); !!}
+    </div>
     <div class="form-group">
 	<label for="youtube_link"> Enter Youtube Link</label> 
 	<input type="text" name="youtube_link" id="youtube_link" placeholder="Enter YouTube Link" class="form-control">
-      </div>
-    <div class="form-group">
-	<label for="slug"> Enter Unique Identifier for the Host</label>
-    	<input type="text" name="slug" id="slug" placeholder="Enter slug" class="form-control">
-      </div>
-
+    </div>
     <div class="form-group">
 	<label for="date"> Enter Date</label>
      	<input type="text" name="date" id="date" placeholder="Enter date" class="form-control">
-      </div>
+    </div>
     <div class="form-group"> 
         {!! Form::submit('Add Day', array('placeholder' => 'btn btn-primary')); !!} 
-
-    </form>
-        {!! Form::close(); !!} 
-  </div>
+    </div>
+  {!! Form::close(); !!} 
 </div>   
 @endsection
